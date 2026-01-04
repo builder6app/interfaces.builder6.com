e02445fa124e77fb0821fb7e19800e66046edb76
Jack Zhuang <9340100@qq.com>
2026-01-04 16:35:02 +0800

    feat: change to liquid

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { join } from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { Liquid } from 'liquidjs';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(
     AppModule,
   );
 
+  const engine = new Liquid();
+  app.engine('liquid', engine.express());
+  app.setViewEngine('liquid');
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.useStaticAssets(join(__dirname, '..', 'public'));
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
